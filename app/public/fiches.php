b0137d7de31b30c2da8334ef1ffa75e206356ea4
joel2035 <joel.pokam@hetic.net>
2018-05-23 19:52:54 +0200

    les fiches

---
+++ app/public/fiches.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+
+  <title></title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="">
+
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <style media="screen">
+  </style>
+</head>
+
+<body>
+  <section class="section">
+    <div class="header-title">
+      <h1 class="header-title">Fiches Pratiques.</h1>
+    </div>
+  </section>
+  <section class="fichesPratique">
+    <div class="container">
+
+      <h1 class="title">Fiches Pratique : Hotels</h1>
+
+      <div class="row">
+
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle13.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+        </div>
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img class="img" src="Rectangle13.png" alt="rectangle13"></a>
+              <p class="text">mon textedjs</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle13.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+
+        </div>
+
+
+
+        <div class="blabla">
+          <button class="bla" type="button" name="button">ajouter plus</button> </div>
+      </div>
+
+
+      <h1 class="title">Fiches Pratique : Restaurants</h1>
+      <div class="row">
+
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle 14.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+        </div>
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img class="img" src="Rectangle 14.png" alt="rectangle13"></a>
+              <p class="text">mon textedjs</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle 14.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+      <div class="blabla">
+        <button class="bla" type="button" name="button">ajouter plus</button> </div>
+
+
+
+      <h1 class="title">Fiches Pratique : Bars</h1>
+      <div class="row">
+
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle15.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+        </div>
+
+
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img class="img" src="Rectangle15.png" alt="rectangle13"></a>
+              <p class="text">mon textedjs</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="four columns">
+          <div class="content">
+            <div class="imgContent">
+              <a href=""><img src="Rectangle15.png" alt="rectangle13"></a>
+            </div>
+            <p class="text">mon textedjs</p>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+      <div class="blabla">
+        <button class="bla" type="button" name="button">ajouter plus</button> </div>
+
+
+
+    </div>
+  </section>
+
+  <body>
+
+    <script src="scripts/main.js"></script>
+    <!-- endbuild -->
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID -->
+    <script>
+      (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+          m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+      ga('create', 'UA-XXXXX-X', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <!-- Built with love using Web Starter Kit -->
+  </body>
+
+</html>
